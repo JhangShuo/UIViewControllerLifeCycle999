2ca408edadf21c3750d86f6640fef41fbf5da243
Jhang wen shuo <jhangwenshuo@Jhangs-MacBook-Air.local>
2015-08-19 11:45:38 +0800

    Initial Commit

---
+++ UIViewControllerLifeCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewControllerLifeCycle
+//
+//  Created by Jhang wen shuo on 8/19/15.
+//  Copyright (c) 2015 Jhang wen shuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIViewControllerLifeCycle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIViewControllerLifeCycle
+//
+//  Created by Jhang wen shuo on 8/19/15.
+//  Copyright (c) 2015 Jhang wen shuo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
